2e02dff66434e3d90386666a080d68c7c6adf545
AppleBoiy <work.chaipat@gmail.com>
2023-10-09 01:58:09 +0700

    Docker Enabling Logging and Log Viewing

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
-from app import views # noqa
+
+
+app = Flask(__name__, static_folder="static")
+
+app.config["DEBUG"] = True
+
+from app import views  # noqa
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
+from flask import jsonify
 from app import app
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Flask says 'Hello world!'"
+
+
+@app.route("/phonebook")
+def index():
+    return app.send_static_file("phonebook.html")
+
+
+@app.route("/api/data")
+def data():
+    d = {"Alice": "(708) 727-2377", "Bob": "(305) 734-0429"}
+    app.logger.debug(str(len(d)) + " entries in phonebook")
+    return jsonify(d)
+
+
+@app.route("/resume")
+def resume():
+    return app.send_static_file("resume.html")
