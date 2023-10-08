2c4714b7a52c44b9b7518fe0c979f04576c0bae3
Chaipat J <work.chaipat@gmail.com>
2023-10-09 02:08:12 +0700

    Update gunicorn.config.py

--- config/gunicorn.config.py
+++ config/gunicorn.config.py
@@ -1,13 +1,23 @@
 # Change directory to specified directory before loading apps.
 chdir = "app"
-# The number of worker processes for handling requests.
-workers = 2
-# The number of worker threads for handling requests.
-threads = 2
-# The socket to bind.
+# Define the host and port for your application.
 bind = ["0.0.0.0:8000"]
+# Number of worker processes.
+workers = 2
+# Number of threads per worker (only applicable to certain worker classes).
+threads = 2
+# Set the worker class. You can use "sync", "eventlet", "gevent", or other options.
+worker_class = "gevent"
+# Timeout for handling a request.
+timeout = 30
+
+# Log settings (customize log file and log level as needed).
+# accesslog = "/path/to/access.log"
+# errorlog = "/path/to/error.log"
+
 # How verbose the Gunicorn error logs should be
 loglevel = "debug"
+
 # default is the send error log to stdout
 # errorlog = '-'
 # accesslog = '-'
@@ -15,3 +25,20 @@ loglevel = "debug"
 capture_output = True
 # hot reload
 reload = True
+
+# Path to the virtual environment, if applicable.
+# Uncomment the line below and replace "/path/to/venv" with your actual virtual environment path.
+# pythonpath = "/path/to/venv"
+
+# Additional environment variables (optional).
+# env = {
+#     "VAR_NAME": "value",
+#     "ANOTHER_VAR": "another_value",
+# }
+
+# SSL settings (uncomment if using HTTPS).
+# keyfile = "/path/to/ssl_key.pem"
+# certfile = "/path/to/ssl_cert.pem"
+
+# Enable graceful restarts.
+# graceful_timeout = 10
